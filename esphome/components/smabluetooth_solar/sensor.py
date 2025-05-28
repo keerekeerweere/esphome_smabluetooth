3ba697d21ffcaba17ddcfd3afad7a74e8ee824f0
rupertbenbrook <rupert@altitudeangel.com>
2025-05-28 20:26:38 +0100

    Increase sensor polling time

--- esphome/components/smabluetooth_solar/sensor.py
+++ esphome/components/smabluetooth_solar/sensor.py
@@ -178,7 +178,7 @@ CONFIG_SCHEMA = (
             ),
         }
     )
-    .extend(cv.polling_component_schema("60s"))
+    .extend(cv.polling_component_schema("10s"))
 #    .extend(modbus.modbus_device_schema(0x01))
 )
 
