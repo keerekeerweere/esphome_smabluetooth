c3593075cc0dc68dd165203468081a9f1141f5fc
keerekeerweere <keerekeerweere@me.compea.com>
2025-07-17 21:18:08 +0200

    bracket

--- esphome/components/smabluetooth_solar/SMA_Inverter.cpp
+++ esphome/components/smabluetooth_solar/SMA_Inverter.cpp
@@ -509,14 +509,15 @@ E_RC ESP32_SMA_Inverter::getInverterDataCfl(uint32_t command, uint32_t first, ui
                   //printUnixTime(timeBuf, datetime);
                   break;
 
-              case NameplateLocation: //INV_NAME
-                  //This function gives us the time when the inverter was switched on
-                  invData.WakeupTime = datetime;
-                  const char* recordChar = (const char*)recptr + 8;
-                  size_t maxLen = (recordsize > 8) ? (recordsize - 8) : 0;
-                  invData.DeviceName = (recordChar, strnlen(recordChar, maxLen));  // Safe copy
-                  ESP_LOGD(TAG, "INV_NAME %s", invData.DeviceName.c_str());
-                  break;
+              case NameplateLocation: { //INV_NAME 
+                    //This function gives us the time when the inverter was switched on
+                    invData.WakeupTime = datetime;
+                    char* recordChar = (char*)(recptr + 8);
+                    size_t maxLen = (recordsize > 8) ? (recordsize - 8) : 0;
+                    invData.DeviceName = (recordChar, strnlen(recordChar, maxLen));  // Safe copy
+                    ESP_LOGD(TAG, "INV_NAME %s", invData.DeviceName.c_str());
+                    break;
+                  }
 
               case NameplatePkgRev: //INV_SWVER
                   invData.SWVersion = get_version(get_u32(recptr + 24));
